937cf5caee96feb952586c87effc4c20b5e111f9
David Berget <davidberget@gmail.com>
2018-06-11 16:20:30 -0500

    fixed redirect issue, updated channel

--- lib/pageless_web/channels/user_channel.ex
+++ lib/pageless_web/channels/user_channel.ex
@@ -44,20 +44,6 @@ defmodule PagelessWeb.UserChannel do
     {:reply, {:ok, %{lessons: lessons}}, socket}
   end
 
-  def handle_in("save_lesson", %{"lesson" => lesson_params}, socket) do
-    params = Map.put(lesson_params, "company_id", socket.assigns[:current_user].user.company_id)
-
-    Pageless.Lessons.create_lesson(params)
-
-    {:reply, :ok, socket}
-  end
-
-  def handle_in("save_file", %{"data" => data}, socket) do
-    IO.inspect(Base.decode64!(data))
-
-    {:reply, :ok, socket}
-  end
-
   defp filter_user(user) do
     %{
       firstName: user.first_name,
@@ -90,31 +76,31 @@ defmodule PagelessWeb.UserChannel do
 
   def filter_steps(%Pageless.Lessons.Lesson{} = step) do
     %{
-      content: step.content,
+      content: step.source,
       description: step.description,
       id: step.id,
       title: step.title,
-      type: step.type
+      type: step.lesson_type
     }
   end
 
   defp filter_lessons(lesson) do
     %{
-      content: lesson.content,
+      content: lesson.source,
       description: lesson.description,
       id: lesson.id,
       title: lesson.title,
-      type: lesson.type
+      type: lesson.lesson_type
     }
   end
 
   defp filter_lesson(lesson) do
     %{
-      content: lesson.content,
+      content: lesson.source,
       description: lesson.description,
       id: lesson.id,
       title: lesson.title,
-      type: lesson.type
+      type: lesson.lesson_type
     }
   end
 end
