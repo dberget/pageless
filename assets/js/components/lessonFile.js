6a5655639d0a9bac50bf70531968ef90c2f563b0
David Berget <davidberget@gmail.com>
2018-06-13 11:06:21 -0500

    small fix to download link

--- assets/js/components/lessonFile.js
+++ assets/js/components/lessonFile.js
@@ -2,28 +2,11 @@ import React from "react"
 import Button from "@material-ui/core/Button"
 import { getCsrfToken } from "../token"
 
-const downloadLesson = id => {
-  const token = getCsrfToken()
-  let url
-
-  fetch(`/api/download/${id}`, {
-    method: "GET",
-    credentials: "same-origin",
-    headers: {
-      "x-csrf-token": token
-    }
-  }).then(resp => resp.blob())
-}
-
 const LessonFile = ({ lesson }) => (
   <div>
     This is a Lesson File.
-    <Button
-      href={`/uploads/${lesson.content.split("/").slice(-1)}`}
-      onClick={() => downloadLesson(lesson.id)}
-    >
-      {" "}
-      Download{" "}
+    <Button href={`/uploads/${lesson.content.split("/").slice(-1)}`}>
+      Download
     </Button>
   </div>
 )
